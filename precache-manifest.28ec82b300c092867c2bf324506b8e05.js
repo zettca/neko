037e129a8fea249f96a94bfa742f2c8123bfef0b
Bruno <zettca@gmail.com>
2019-10-13 14:48:05 +0100

    Updates

---
+++ precache-manifest.28ec82b300c092867c2bf324506b8e05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03b865cfb0028560024079a0fe084ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c32d7bfe10bc7f4e2be8",
+    "url": "/static/css/main.d1c2dd69.chunk.css"
+  },
+  {
+    "revision": "81d74e7682454107a135",
+    "url": "/static/js/2.76ad931c.chunk.js"
+  },
+  {
+    "revision": "c32d7bfe10bc7f4e2be8",
+    "url": "/static/js/main.a0952479.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  },
+  {
+    "revision": "e4f0b22bdc09e0fee7f32f360c46727c",
+    "url": "/static/media/calendar.e4f0b22b.svg"
+  },
+  {
+    "revision": "6c24478b42a76a52b34327c60136886b",
+    "url": "/static/media/neko.6c24478b.svg"
+  }
+]);
